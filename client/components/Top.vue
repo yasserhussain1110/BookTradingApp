df5043517e227d3b084d42fd0c1fcc10955da19f
Yasser Hussain <yasserhussain1110@gmail.com>
2017-03-22 15:24:13 +0530

    Started UI

---
+++ client/components/Top.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="top">
+    <h1>{{ msg }}</h1>
+    <div>
+      <button>Login</button>
+      <button>SignUp</button>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'top',
+    data () {
+      return {
+        msg: 'Book Trading App'
+      }
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
