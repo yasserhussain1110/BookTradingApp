5d8e508c3a5befb23364b9b766a69ddef1549897
Yasser Hussain <yasserhussain1110@gmail.com>
2017-03-24 19:28:33 +0530

    Moved API routing.
    Moved API routing above 'webpack-dev-server' routing.
    Otherwise webpack-dev-server will consume requests
    for all urls even those meant for the API.
    For eg, it accepted the request for url - '/books'
    
    The reason for this is 'connect-history-api-fallback',
    which allows 'webpack-dev-server' to fallback to
    'index.html' for any unrecognized url.
    
    Removing 'connect-history-api-fallback' would also have
    fixed the issue. (i.e. made 'webpack-dev-server' leave
    requests coming to API urls, allowing them to be handled
    by API request handlers registered in the 'server.js'
    file.)
    
    But I have allowed 'connect-history-api-fallback' to
    remain and instead moved API routes above
    'webpack-dev-sever' routing because
    'connect-history-api-fallback' might be useful later.
    
    See -
    http://stackoverflow.com/questions/26203725/how-to-allow-for-webpack-dev-server-to-allow-entry-points-from-react-router#28623662

--- server/server.js
+++ server/server.js
@@ -9,16 +9,20 @@ const app = express();
 
 app.use(bodyParser.json());
 
-if (process.env.NODE_ENV === "development") {
-  console.log("Running In Development");
-  require('./tools/setupDev')(app);
-} else if (process.env.NODE_ENV === "production") {
-  console.log("Running In Production");
-}
-
+/* Set up API / Other Backend Routes */
 require('./routes/userRoutes')(app);
 require('./routes/bookRoutes')(app);
 require('./routes/tradeRequestRoutes')(app);
+
+/* Set up development server if required */
+if (process.env.NODE_ENV === "development") {
+  console.log("Running In Development");
+  require('./tools/setupDev')(app);
+
+  /* Or serve static assets in production */
+} else if (process.env.NODE_ENV === "production") {
+  console.log("Running In Production");
+}
 
 app.listen(port, () => {
   console.log(`Server started on ${port}`);
